9efb84645c4bf78fd4020c61e2b55d524661296f
Breno Batista Campos <brenobatistacampos@gmail.com>
2022-11-04 00:27:58 -0300

    tabela usuarios e autenticação adicionados

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 def inserir_pessoas():
     pessoa = Pessoas(nome='Breno', idade=25)
@@ -22,8 +22,19 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Breno').first()
     pessoa.delete()
 
+def inserir_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
 if __name__ == '__main__':
+    # inserir_usuario('breno', '123')
+    # inserir_usuario('campos', '321')
+    consulta_todos_usuarios()
     # inserir_pessoas()
     # exclui_pessoa()
-    consulta()
+    # consulta()
     # altera_dados()
